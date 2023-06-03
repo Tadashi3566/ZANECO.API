0eb0091ca41ec9f1adbab32aaf4a37cd4b7a1f9f
Kirkey Psalms <kirkey@outlook.ph>
2023-06-03 11:43:30 +0800

    added imagepath checking if existing

---
+++ src/Core/Application/ISD/HR/EmployeeManager/TimeLogs/TimeLogByFileNameSpec.cs
@@ -0,0 +1,8 @@
+﻿using ZANECO.API.Domain.ISD.HR.EmployeeManager;
+
+namespace ZANECO.API.Application.ISD.HR.EmployeeManager.TimeLogs;
+
+public class TimeLogByFileNameSpec : Specification<TimeLog, TimeLogDto>, ISingleResultSpecification
+{
+    public TimeLogByFileNameSpec(string fileName) => Query.Where(p => string.Concat(p.ImagePath).Contains(fileName));
+}
